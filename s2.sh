3461f6cea7675814d5ea3c27d037b7377c2d2771
kannankvs <kannan_kvs@dell.com>
2021-10-21 00:13:04 +0530

    Create s2.sh

---
+++ s2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin bash -e
+git checkout -b br2
+git config --global user.email "Kannan_KVS@Dell.com"
+git config --global user.name "kannankvs"
+git pull --ff-only origin br2
+echo '{ KVSK2-Prasanna2 - is it working now4?' > s1.json
+echo "\n}" >> s1.json
+git add s1.json
+git commit -m "latest links for sonic images"
+git push -f --set-upstream origin br2
