ca65f36774ee5d321b63fcd2a6420b4e68feac6c
kannankvs <kannan_kvs@dell.com>
2021-10-20 18:34:48 +0530

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -7,6 +7,7 @@ git config --global user.name "kannankvs"
 git push origin br2
 echo '{ KVSK1-Prasanna - S1 - Try1' > s1.json
 echo "\n}" >> s1.json
+git status
 git add s1.json
 git commit -m "latest links for sonic images"
 git push --set-upstream origin br2
