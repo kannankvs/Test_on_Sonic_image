02e38ad2c2da35e91b0739d91b66b1a91b5f05aa
kannankvs <kannan_kvs@dell.com>
2021-10-20 19:04:42 +0530

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -8,7 +8,7 @@ git config --global user.email "Kannan_KVS@Dell.com"
 git config --global user.name "kannankvs"
 echo '{ KVSK1-Prasanna1 - S1 - Try1' > s1.json
 echo "\n}" >> s1.json
-ECHO GIT add
+echo GIT add
 git add s1.json
 git commit -m "latest links for sonic images"
 echo GIT last push
