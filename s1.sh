a2ab329f5314b906ecf1e3f4daba1b7c737fc8c5
kannankvs <kannan_kvs@dell.com>
2021-10-20 20:20:37 +0530

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -3,8 +3,7 @@ git checkout -b br2
 git config --global user.email "Kannan_KVS@Dell.com"
 git config --global user.name "kannankvs"
 git pull --ff-only origin br2
-git push origin br2
-echo '{ KVSK2-Prasanna2 - is it working now3?' > s1.json
+echo '{ KVSK2-Prasanna2 - is it working now4?' > s1.json
 echo "\n}" >> s1.json
 git add s1.json
 git commit -m "latest links for sonic images"
