6030635bb189dec9490d0140e81e8f28b15ab560
kannankvs <kannan_kvs@dell.com>
2021-10-20 20:15:33 +0530

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -2,7 +2,7 @@
 git checkout -b br2
 git config --global user.email "Kannan_KVS@Dell.com"
 git config --global user.name "kannankvs"
-git pull origin br2
+git pull --ff-only origin br2
 git push origin br2
 echo '{ KVSK2-Prasanna2 - is it working now2?' > s1.json
 echo "\n}" >> s1.json
