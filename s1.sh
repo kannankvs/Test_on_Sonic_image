30250b860a3ae5e6cb032f1f27b29e0b4bbc6826
kannankvs <kannan_kvs@dell.com>
2021-10-20 19:00:58 +0530

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -1,14 +1,8 @@
 #!/usr/bin bash -e
 echo GIT CHECKOUT MAIN
 git checkout main
-echo GIT fetch origin
-git fetch origin 
-echo GIT hard 
-git reset --hard origin/main
-echo GIT CHECKOUT br2
-git checkout -b br2
 echo GIT pull
-git pull br2
+git pull origin br2
 echo GIT config
 git config --global user.email "Kannan_KVS@Dell.com"
 git config --global user.name "kannankvs"
